95fd3f0aaf3ca242892ad8d1dc2cf9fb18afb1b9
Pasha <54247708+DeathBarmaglot@users.noreply.github.com>
2022-01-18 16:59:15 +0200

    Update index_edgePreload.js

--- index_edgePreload.js
+++ index_edgePreload.js
@@ -49,9 +49,9 @@ if(document.addEventListener ){
    htFallbacks={
    };
 
-   aLoader = [
-      { load: "edge_includes/jquery-2.0.3.min.js"},
-      { load: "edge_includes/edge.4.0.0.min.js"},
+     aLoader = [
+{ load: "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"},
+      { load: "https://molbuk.ua/edge.4.0.0.min.js"},
       { load: "index_edge.js"},
       { load: "index_edgeActions.js"}];
 
